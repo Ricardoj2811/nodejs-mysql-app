8753a743ee5e6935981c066a79fa2da7b5801f76
Ricardo Nuñez <ricardoj2811@gmail.com>
2022-11-14 17:47:29 -0300

    CRUD and login with node.js and MySQL

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database:{
+        host: 'localhost',
+        port:3307,
+        database: 'database_links',
+        user: 'root',
+        password: '',
+    }
+}
---
+++ src/routes/links.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../database');
+const { isLoggedIn } = require('../lib/auth');
+
+router.get('/add', isLoggedIn, (req, res) => {
+    res.render('links/add');
+})
+
+router.post('/add',isLoggedIn, async (req, res) => {
+    const {title, url, description} = req.body;
+    const newLink ={
+        title,
+        url, 
+        description, 
+        user_id: req.user.id
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    req.flash('success', 'Link saved successfully');
+    res.redirect('/links');
+    
+});
+
+router.get('/', isLoggedIn, async (req, res) => {
+    const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    res.render('links/list', {links});
+});
+
+router.get('/delete/:id', isLoggedIn, async (req, res) => {
+    const {id} = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+    req.flash('success', 'Links Removed Successfully');
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
+    const {id} = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', isLoggedIn, async (req, res) => {
+    const { id } = req.params;
+    
+    const {title, url, description} = req.body;
+    const newID = id.substring(1,id.length)
+    const newLink ={
+        title,
+        url, 
+        description
+    };
+    req.flash('success', 'Link Update Successfully');
+    await pool.query('UPDATE links SET ? WHERE id = ?', [newLink, newID]);
+    res.redirect('/links');
+});
+
+module.exports = router;
